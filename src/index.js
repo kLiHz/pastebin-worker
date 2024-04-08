c3555e32f15b67bcfdf629c8414c33e6d63aad88
kLiHz <66966137+kLiHz@users.noreply.github.com>
2024-04-08 17:29:57 +0800

    Recover cache practice

--- src/index.js
+++ src/index.js
@@ -171,9 +171,11 @@ async function handleGet(request, env, ctx) {
     return Response.redirect(env.FAVICON)
   }
 
-  const cacheKey = new Request(new URL((passwd.length > 0) ? "/" : url.pathname, env.BASE_URL));
-  const cachedResponse = await caches.default.match(cacheKey);
-  if (cachedResponse) { return cachedResponse; }
+  if (passwd.length > 0 || url.pathname == '/' || url.pathname == '/tos' || url.pathname == '/api' || url.pathname.startsWith('/favicon.')) {
+    const cacheKey = new Request(new URL((passwd.length > 0) ? "/" : url.pathname, env.BASE_URL), request);
+    const cachedResponse = await caches.default.match(cacheKey);
+    if (cachedResponse) { return cachedResponse; }
+  }
 
   if (url.pathname == '/index' || url.pathname == '/index.html') {
     return Response.redirect(new URL('/', request.url));
